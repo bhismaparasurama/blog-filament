5870f2a53171029364c8d43a80012de2f75de8cc
bhismaraA <bhismaparasurama2023@gmail.com>
2025-08-26 17:00:27 +0800

    maybee this workkk finalyyy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,5 +23,17 @@ class AppServiceProvider extends ServiceProvider
         if (str_starts_with(config('app.url'), 'https://')) {
             URL::forceScheme('https');
         }
+
+        if (env('UPLOAD_MAX_FILESIZE')) {
+            ini_set('upload_max_filesize', env('UPLOAD_MAX_FILESIZE'));
+        }
+
+        if (env('POST_MAX_SIZE')) {
+            ini_set('post_max_size', env('POST_MAX_SIZE'));
+        }
+
+        if (env('MEMORY_LIMIT')) {
+            ini_set('memory_limit', env('MEMORY_LIMIT'));
+        }
     }
 }
