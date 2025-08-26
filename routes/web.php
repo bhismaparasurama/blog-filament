2456089866d741ff87970eabe77d703e739b0899
bhismaraA <bhismaparasurama2023@gmail.com>
2025-08-26 17:30:08 +0800

    tesss semoga work

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/php-info', function () {
+    return [
+        'upload_max_filesize' => ini_get('upload_max_filesize'),
+        'post_max_size' => ini_get('post_max_size'),
+        'memory_limit' => ini_get('memory_limit'),
+    ];
+});
