127d6472fbeca0fbc9f22d009ad4f37e217acc64
bhismaraA <bhismaparasurama2023@gmail.com>
2025-08-26 19:58:45 +0800

    done gak sieh

--- config/cors.php
+++ config/cors.php
@@ -7,6 +7,7 @@ return [
     'allowed_origins' => [
         'http://localhost:3000',   // Next.js local dev
         'http://127.0.0.1:3000',
+        'https://bhisma-cv-portfolio.vercel.app/',
     ],
 
     'allowed_origins_patterns' => [],
